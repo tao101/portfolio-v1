92c0b79a04ee62e287cf85617fa615ad2ea87098
taoufiq lotfi <leh.lotfi@gmail.com>
2020-08-16 20:48:36 +0300

    working on the languages section

--- src/componenets/Education.js
+++ src/componenets/Education.js
@@ -2,12 +2,24 @@ import React from "react";
 import Colors from "../assets/colors";
 import "./Education.css";
 import { Card, CardContent, Typography, Avatar } from "@material-ui/core";
-import c from "programming-languages-logos/src/c/c.svg";
+import csharp from "programming-languages-logos/src/csharp/csharp.svg";
 import cpp from "programming-languages-logos/src/cpp/cpp.svg";
 import css from "../assets/css.svg";
+import react from "../assets/react.svg";
+import mobx from "../assets/mobx.svg";
+import reactNative from "../assets/reactNative.png";
 import html from "programming-languages-logos/src/html/html.svg";
 import java from "programming-languages-logos/src/java/java.svg";
+import swift from "programming-languages-logos/src/swift/swift.svg";
 import javascript from "programming-languages-logos/src/javascript/javascript.svg";
+import nodejs from "../assets/nodejs-icon.svg";
+import electron from "../assets/electron.svg";
+import git from "../assets/git-icon.svg";
+import bootstarp from "../assets/bootstrap.svg";
+import parse from "../assets/parse.svg";
+import mysql from "../assets/mysql.svg";
+import firebase from "../assets/firebase.svg";
+import expo from "../assets/expo.svg";
 
 export default class Education extends React.Component {
   render() {
@@ -53,22 +65,270 @@ export default class Education extends React.Component {
             </CardContent>
           </Card>
         </div>
-        <div className="col-sm item">
+        <div className="col-sm item ">
           <h3 style={{ color: Colors(darkMode, "headline") }}>
             Language And Frameworks I speak
           </h3>
-          <div className="row languages">
-            <div className="avatarContainer">
-              <Avatar className="avatar" alt="c" src={c} />
+          <div className="centerX">
+            <div className="row">
+              <div className="col-2 logos">
+                <img
+                  className="avatar img-fluid"
+                  alt="cpp"
+                  src={cpp}
+                  title="c++"
+                />
+                <h4
+                  className="name"
+                  style={{ color: Colors(darkMode, "headline") }}
+                >
+                  c++
+                </h4>
+              </div>
+              <div className="col-2 logos">
+                <img
+                  className="avatar img-fluid"
+                  alt="csharp"
+                  src={csharp}
+                  title="c#"
+                />
+                <h4
+                  className="name"
+                  style={{ color: Colors(darkMode, "headline") }}
+                >
+                  c#
+                </h4>
+              </div>
+              <div className="col-2 logos">
+                <img
+                  className="avatar img-fluid"
+                  alt="java"
+                  src={javascript}
+                  title="javascript"
+                />
+                <h4
+                  className="name"
+                  style={{ color: Colors(darkMode, "headline") }}
+                >
+                  javascript
+                </h4>
+              </div>
+              <div className="col-2 logos">
+                <img
+                  className="avatar img-fluid"
+                  alt="java"
+                  src={java}
+                  title="java"
+                />
+                <h4
+                  className="name"
+                  style={{ color: Colors(darkMode, "headline") }}
+                >
+                  java
+                </h4>
+              </div>
+              <div className="col-2 logos">
+                <img
+                  className="avatar img-fluid"
+                  alt="swift"
+                  src={swift}
+                  title="swift"
+                />
+                <h4
+                  className="name"
+                  style={{ color: Colors(darkMode, "headline") }}
+                >
+                  swift
+                </h4>
+              </div>
             </div>
-            <div className="avatarContainer">
-              <Avatar className="avatar" alt="cpp" src={cpp} />
+            <div className="row">
+              <div className="col-2 logos">
+                <img
+                  className="avatar img-fluid"
+                  alt="html"
+                  src={html}
+                  title="html"
+                />
+                <h4
+                  className="name"
+                  style={{ color: Colors(darkMode, "headline") }}
+                >
+                  html
+                </h4>
+              </div>
+              <div className="col-2 logos">
+                <img
+                  className="avatar img-fluid"
+                  alt="css"
+                  src={css}
+                  title="css"
+                />
+                <h4
+                  className="name"
+                  style={{ color: Colors(darkMode, "headline") }}
+                >
+                  css
+                </h4>
+              </div>
+              <div className="col-2 logos">
+                <img
+                  className="avatar img-fluid"
+                  alt="bootstarp"
+                  src={bootstarp}
+                  title="bootstarp"
+                />
+                <h4
+                  className="name"
+                  style={{ color: Colors(darkMode, "headline") }}
+                >
+                  bootstarp
+                </h4>
+              </div>
+              <div className="col-2 logos">
+                <img
+                  className="avatar img-fluid"
+                  alt="react"
+                  src={react}
+                  title="react"
+                />
+                <h4
+                  className="name"
+                  style={{ color: Colors(darkMode, "headline") }}
+                >
+                  react
+                </h4>
+              </div>
+              <div className="col-2 logos">
+                <img
+                  className="avatar img-fluid"
+                  alt="mobx"
+                  src={mobx}
+                  title="mobx"
+                />
+                <h4
+                  className="name"
+                  style={{ color: Colors(darkMode, "headline") }}
+                >
+                  mobx
+                </h4>
+              </div>
             </div>
-            <div className="avatarContainer">
-              <Avatar className="avatar" alt="javascript" src={javascript} />
+            <div className="row">
+              <div className="col-2 logos">
+                <img
+                  className="avatar img-fluid"
+                  alt="reactNative"
+                  src={reactNative}
+                  title="reactNative"
+                />
+                <h4
+                  className="name"
+                  style={{ color: Colors(darkMode, "headline") }}
+                >
+                  react Native
+                </h4>
+              </div>
+              <div className="col-2 logos">
+                <img
+                  className="avatar img-fluid"
+                  alt="expo"
+                  src={expo}
+                  title="expo"
+                />
+                <h4
+                  className="name"
+                  style={{ color: Colors(darkMode, "headline") }}
+                >
+                  expo
+                </h4>
+              </div>
+              <div className="col-2 logos">
+                <img
+                  className="avatar img-fluid"
+                  alt="electron"
+                  src={electron}
+                  title="electron"
+                />
+                <h4
+                  className="name"
+                  style={{ color: Colors(darkMode, "headline") }}
+                >
+                  electron
+                </h4>
+              </div>
+              <div className="col-2 logos">
+                <img
+                  className="avatar img-fluid"
+                  alt="nodejs"
+                  src={nodejs}
+                  title="nodejs"
+                />
+                <h4
+                  className="name"
+                  style={{ color: Colors(darkMode, "headline") }}
+                >
+                  nodejs
+                </h4>
+              </div>
             </div>
-            <div className="avatarContainer">
-              <Avatar className="avatar" alt="java" src={java} />
+            <div className="row">
+              <div className="col-2 logos">
+                <img
+                  className="avatar img-fluid"
+                  alt="parse"
+                  src={parse}
+                  title="parse"
+                />
+                <h4
+                  className="name"
+                  style={{ color: Colors(darkMode, "headline") }}
+                >
+                  parse server
+                </h4>
+              </div>
+              <div className="col-2 logos">
+                <img
+                  className="avatar img-fluid"
+                  alt="mysql"
+                  src={mysql}
+                  title="mysql"
+                />
+                <h4
+                  className="name"
+                  style={{ color: Colors(darkMode, "headline") }}
+                >
+                  mysql
+                </h4>
+              </div>
+              <div className="col-2 logos">
+                <img
+                  className="avatar img-fluid"
+                  alt="firebase"
+                  src={firebase}
+                  title="firebase"
+                />
+                <h4
+                  className="name"
+                  style={{ color: Colors(darkMode, "headline") }}
+                >
+                  firebase
+                </h4>
+              </div>
+              <div className="col-2 logos">
+                <img
+                  className="avatar img-fluid"
+                  alt="git"
+                  src={git}
+                  title="git"
+                />
+                <h4
+                  className="name"
+                  style={{ color: Colors(darkMode, "headline") }}
+                >
+                  git
+                </h4>
+              </div>
             </div>
           </div>
         </div>
